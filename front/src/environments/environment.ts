1c51616f3a012c798744c8c7dd4759bb68384792
Rémi Aubert <aubert.remi@gmail.com>
2023-03-05 09:34:36 +0100

    fix: hardcoded kc values

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   graphqlUrl: "/graphql",
-  keycloakUrl: '#{KEYCLOAK_URL}',
-  keycloakRealm: '#{KEYCLOAK_REALM}',
-  keycloakClientId: '#{KEYCLOAK_CLIENT_ID}'
+  keycloakUrl: 'https://app-38fe6a67-2a9e-4e27-b735-b3dd1c3e4698.cleverapps.io',
+  keycloakRealm: 'JOURNALIST-CRM',
+  keycloakClientId: 'journalist-crm-front',
 };
